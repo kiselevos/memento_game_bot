40837d08fb60d9c8cbbd4d581e0f8fa09b41cd23
kiselevos <olegsergeevichkiselev@gmail.com>
2025-11-01 22:08:42 +0800

    fix bug with logs create file

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -3,9 +3,18 @@ package logging
 import (
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func InitLogger(logFilePath string) {
+
+	dir := filepath.Dir(logFilePath)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			log.Fatalf("Не удалось создать директорию логов: %v", err)
+		}
+	}
+
 	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("Не удалось открыть лог-файл: %v", err)
